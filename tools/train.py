3b7edd3daf641ecc566578107e167e8aa0c69b29
ps <ps@ps>
2021-06-01 17:18:50 +0800

    1

--- tools/train.py
+++ tools/train.py
@@ -1,5 +1,5 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 import sys
 import argparse
 import torch
@@ -172,9 +172,9 @@ def run_train():
     os.environ['NCCL_IB_DISABLE'] = '1'
     # os.environ['NCCL_DEBUG'] = 'INFO'
 
-    # args = parser.parse_args()
-    args = parser.parse_args(['--model_dir', 'retina_fpn_vpd_nos_non',
-                              '--resume_weights', '24'])
+    args = parser.parse_args()
+    # args = parser.parse_args(['--model_dir', 'retina_fpn_vpd_nos_non',
+    #                           '--resume_weights', '24'])
 
     # import libs
     model_root_dir = os.path.join(model_dir, args.model_dir)
