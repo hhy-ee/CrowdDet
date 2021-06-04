7d967e9de564467a43ec1d369456f38a3c91b3b8
gpu-4 <you@example.com>
2021-06-05 01:37:53 +0800

    1

--- tools/train.py
+++ tools/train.py
@@ -168,12 +168,12 @@ def run_train():
     parser.add_argument('--model_dir', '-md', default=None,required=True,type=str)
     parser.add_argument('--resume_weights', '-r', default=None,type=int)
     os.environ['MASTER_ADDR'] = '127.0.0.1'
-    os.environ['MASTER_PORT'] = '8888'
+    os.environ['MASTER_PORT'] = '8889'
     os.environ['NCCL_IB_DISABLE'] = '1'
     # os.environ['NCCL_DEBUG'] = 'INFO'
 
     args = parser.parse_args()
-    # args = parser.parse_args(['--model_dir', 'retina_fpn_baseline'])
+    # args = parser.parse_args(['--model_dir', 'retina_fpn_nvpd_kll1e-3'])
     # args = parser.parse_args(['--model_dir', 'rcnn_fpn_vpd'])
 
     # import libs
