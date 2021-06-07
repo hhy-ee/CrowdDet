086921571893b76092b947e9fd3759032d57b8d0
hhy <hhy@hhy>
2021-06-07 18:55:53 +0800

    1

--- tools/train.py
+++ tools/train.py
@@ -172,8 +172,8 @@ def run_train():
     os.environ['NCCL_IB_DISABLE'] = '1'
     # os.environ['NCCL_DEBUG'] = 'INFO'
 
-    # args = parser.parse_args()
-    args = parser.parse_args(['--model_dir', 'retina_fpn_baseline'])
+    args = parser.parse_args()
+    # args = parser.parse_args(['--model_dir', 'retina_fpn_baseline'])
     # args = parser.parse_args(['--model_dir', 'rcnn_fpn_vpd'])
 
     # import libs
