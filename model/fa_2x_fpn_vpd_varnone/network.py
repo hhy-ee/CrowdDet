f7ce74cbeb5e2a37d98476af3043f4ad55b910b6
hhy <hhy@hhy>
2021-10-18 19:36:52 +0800

    1

--- model/fa_2x_fpn_vpd_varnone/network.py
+++ model/fa_2x_fpn_vpd_varnone/network.py
@@ -153,7 +153,7 @@ class RetinaNet_Head(nn.Module):
             _.permute(0, 2, 3, 1).reshape(pred_cls[0].shape[0], -1, config.num_classes-1)
             for _ in pred_refined_cls]
         pred_reg_list = [
-            _.permute(0, 2, 3, 1).reshape(pred_reg[0].shape[0], -1, 8)
+            _.permute(0, 2, 3, 1).reshape(pred_reg[0].shape[0], -1, 4)
             for _ in pred_reg]
         return pred_cls_list, pred_refined_cls_list, pred_reg_list
 
@@ -161,13 +161,11 @@ def per_layer_inference(anchors_list, pred_cls_list, pred_reg_list, im_info):
     keep_anchors = []
     keep_cls = []
     keep_reg = []
-    keep_lstd = []
     class_num = pred_cls_list[0].shape[-1]
     for l_id in range(len(anchors_list)):
         anchors = anchors_list[l_id].reshape(-1, 4)
         pred_cls = pred_cls_list[l_id][0].reshape(-1, class_num)
-        pred_reg = pred_reg_list[l_id][0].reshape(-1, 8)[:, :4]
-        pred_lstd = pred_reg_list[l_id][0].reshape(-1, 8)[:, 4:]
+        pred_reg = pred_reg_list[l_id][0].reshape(-1, 4)
         if len(anchors) > config.test_layer_topk:
             ruler = pred_cls.max(axis=1)[0]
             _, inds = ruler.topk(config.test_layer_topk, dim=0)
@@ -175,16 +173,13 @@ def per_layer_inference(anchors_list, pred_cls_list, pred_reg_list, im_info):
             keep_anchors.append(anchors[inds])
             keep_cls.append(torch.sigmoid(pred_cls[inds]))
             keep_reg.append(pred_reg[inds])
-            keep_lstd.append(pred_lstd[inds])
         else:
             keep_anchors.append(anchors)
             keep_cls.append(torch.sigmoid(pred_cls))
             keep_reg.append(pred_reg)
-            keep_lstd.append(pred_lstd)
     keep_anchors = torch.cat(keep_anchors, axis = 0)
     keep_cls = torch.cat(keep_cls, axis = 0)
     keep_reg = torch.cat(keep_reg, axis = 0)
-    keep_lstd = torch.cat(keep_lstd, axis = 0)
     # multiclass
     tag = torch.arange(class_num).type_as(keep_cls)+1
     tag = tag.repeat(keep_cls.shape[0], 1).reshape(-1,1)
@@ -194,7 +189,7 @@ def per_layer_inference(anchors_list, pred_cls_list, pred_reg_list, im_info):
     pred_bbox = restore_bbox(keep_anchors, keep_reg, False)
     pred_bbox = pred_bbox.repeat(1, class_num).reshape(-1, 4)
     if config.save_data or config.test_nms_method == 'kl_nms':
-        pred_bbox = torch.cat([pred_bbox, pred_scores, tag, keep_lstd], axis=1)
+        pred_bbox = torch.cat([pred_bbox, pred_scores, tag], axis=1)
     else:
         pred_bbox = torch.cat([pred_bbox, pred_scores, tag], axis=1)
     return pred_bbox
