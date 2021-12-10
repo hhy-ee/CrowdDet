79f330512a97d8989b32c3f5c2c2986836fc0b64
ps <ps@ps>
2021-12-10 23:36:57 +0800

    1

--- model/retina_fpn_vpd_kll1e-0_xywh/network.py
+++ model/retina_fpn_vpd_kll1e-0_xywh/network.py
@@ -8,9 +8,9 @@ from config import config
 from backbone.resnet50 import ResNet50
 from backbone.fpn import FPN
 from det_oprs.anchors_generator import AnchorGenerator
-from det_oprs.atss_anchor_target import atss_anchor_target, centerness_target
+from det_oprs.retina_anchor_target import retina_anchor_target
 from det_oprs.bbox_opr import bbox_transform_inv_opr
-from det_oprs.loss_opr import focal_loss, giou_loss, kldivergence_loss
+from det_oprs.loss_opr import focal_loss, smooth_l1_loss, kldivergence_loss
 from det_oprs.utils import get_padded_tensor
 
 class Network(nn.Module):
@@ -31,17 +31,17 @@ class Network(nn.Module):
         # stride: 128,64,32,16,8, p7->p3
         fpn_fms = self.FPN(image)
         anchors_list = self.R_Anchor(fpn_fms)
-        pred_cls_list, pred_reg_list, pred_ctn_list = self.R_Head(fpn_fms)
+        pred_cls_list, pred_reg_list = self.R_Head(fpn_fms)
         # release the useless data
         if self.training:
             loss_dict = self.R_Criteria(
-                    pred_cls_list, pred_reg_list, pred_ctn_list, anchors_list, gt_boxes, im_info)
+                    pred_cls_list, pred_reg_list, anchors_list, gt_boxes, im_info)
             return loss_dict
         else:
             #pred_bbox = union_inference(
             #        anchors_list, pred_cls_list, pred_reg_list, im_info)
             pred_bbox = per_layer_inference(
-                    anchors_list, pred_cls_list, pred_reg_list, pred_ctn_list, im_info)
+                    anchors_list, pred_cls_list, pred_reg_list, im_info)
             return pred_bbox.cpu().detach()
 
 class RetinaNet_Anchor():
@@ -68,29 +68,24 @@ class RetinaNet_Criteria(nn.Module):
         self.loss_normalizer = 100 # initialize with any reasonable #fg that's not too small
         self.loss_normalizer_momentum = 0.9
 
-    def __call__(self, pred_cls_list, pred_reg_list, pred_ctn_list, anchors_list, gt_boxes, im_info):
-        num_levels = [int(cls.shape[1]) for cls in pred_cls_list]
+    def __call__(self, pred_cls_list, pred_reg_list, anchors_list, gt_boxes, im_info):
         all_anchors = torch.cat(anchors_list, axis=0)
         all_pred_cls = torch.cat(pred_cls_list, axis=1).reshape(-1, config.num_classes-1)
-        all_pred_ctn = torch.cat(pred_ctn_list, axis=1).reshape(-1)
+        all_pred_cls = torch.sigmoid(all_pred_cls)
         all_pred_dist = torch.cat(pred_reg_list, axis=1).reshape(-1, 8)
         # gaussian reparameterzation
         all_pred_mean = all_pred_dist[:, :4]
         all_pred_lstd = all_pred_dist[:, 4:]
         all_pred_reg = all_pred_mean + all_pred_lstd.exp() * torch.randn_like(all_pred_mean)
         # get ground truth
-        labels, bbox_target = atss_anchor_target(all_anchors, gt_boxes, num_levels, im_info)
+        labels, bbox_target = retina_anchor_target(all_anchors, gt_boxes, im_info, top_k=1)
+        # regression loss
         fg_mask = (labels > 0).flatten()
         valid_mask = (labels >= 0).flatten()
-        anchor_target = all_anchors.repeat(config.train_batch_per_gpu, 1)[fg_mask]
-        ctn_target = centerness_target(anchor_target, bbox_target[fg_mask])
-        # regression loss
-        loss_ctn = F.binary_cross_entropy_with_logits(
-                all_pred_ctn[fg_mask], ctn_target)
-        loss_reg = giou_loss( 
+        loss_reg = smooth_l1_loss(
                 all_pred_reg[fg_mask],
                 bbox_target[fg_mask],
-                anchor_target)
+                config.smooth_l1_beta)
         loss_cls = focal_loss(
                 all_pred_cls[valid_mask],
                 labels[valid_mask],
@@ -104,15 +99,13 @@ class RetinaNet_Criteria(nn.Module):
         self.loss_normalizer = self.loss_normalizer_momentum * self.loss_normalizer + (
             1 - self.loss_normalizer_momentum
             ) * max(num_pos_anchors, 1)
-        loss_ctn = loss_ctn.sum() / self.loss_normalizer
         loss_reg = loss_reg.sum() / self.loss_normalizer
         loss_cls = loss_cls.sum() / self.loss_normalizer
         loss_kld = loss_kld.sum() / self.loss_normalizer
         loss_dict = {}
-        loss_dict['atss_focal_loss'] = loss_cls
-        loss_dict['atss_smooth_l1'] = loss_reg
-        loss_dict['atss_centerness'] = loss_ctn
-        loss_dict['atss_loss_kld'] = loss_kld
+        loss_dict['retina_focal_loss'] = loss_cls
+        loss_dict['retina_smooth_l1'] = loss_reg
+        loss_dict['retina_loss_kld'] = loss_kld
         return loss_dict
 
 class RetinaNet_Head(nn.Module):
@@ -120,11 +113,6 @@ class RetinaNet_Head(nn.Module):
         super().__init__()
         num_convs = 4
         in_channels = 256
-        reg_channels = 64
-        if config.stat_mode == 'std':
-            ref_channels = 4
-        elif config.stat_mode == 'pdf':
-            ref_channels = 20
         cls_subnet = []
         bbox_subnet = []
         for _ in range(num_convs):
@@ -138,13 +126,6 @@ class RetinaNet_Head(nn.Module):
             bbox_subnet.append(nn.ReLU(inplace=True))
         self.cls_subnet = nn.Sequential(*cls_subnet)
         self.bbox_subnet = nn.Sequential(*bbox_subnet)
-
-        # refinement
-        conf_vector = [nn.Conv2d(ref_channels, reg_channels, 1)]
-        conf_vector += [nn.ReLU(inplace=True)]
-        conf_vector += [nn.Conv2d(reg_channels, 1, 1), nn.Sigmoid()]
-        self.reg_conf = nn.Sequential(*conf_vector)
-
         # predictor
         self.cls_score = nn.Conv2d(
             in_channels, config.num_cell_anchors * (config.num_classes-1),
@@ -152,12 +133,10 @@ class RetinaNet_Head(nn.Module):
         self.bbox_pred = nn.Conv2d(
             in_channels, config.num_cell_anchors * 8,
             kernel_size=3, stride=1, padding=1)
-        self.centerness_pred = nn.Conv2d(
-            in_channels, config.num_cell_anchors * 1,
-            kernel_size=3, stride=1, padding=1)
+
         # Initialization
         for modules in [self.cls_subnet, self.bbox_subnet, self.cls_score, 
-                        self.bbox_pred, self.centerness_pred, self.reg_conf]:
+                        self.bbox_pred]:
             for layer in modules.modules():
                 if isinstance(layer, nn.Conv2d):
                     torch.nn.init.normal_(layer.weight, mean=0, std=0.01)
@@ -170,29 +149,9 @@ class RetinaNet_Head(nn.Module):
     def forward(self, features):
         pred_cls = []
         pred_reg = []
-        pred_ctn = []
         for feature in features:
-            cls_score = self.cls_score(self.cls_subnet(feature))
-            bbox_pred = self.bbox_pred(self.bbox_subnet(feature))
-            # refinement
-            if config.stat_mode == 'std':
-                stat = bbox_pred[:, 4:]
-            elif config.stat_mode == 'pdf':
-                N, _, H, W = bbox_pred.size()
-                mean = bbox_pred[:, :4].permute(0,2,3,1).reshape(-1, 1)
-                lstd = bbox_pred[:, 4:].permute(0,2,3,1).reshape(-1, 1)
-                q0 = torch.distributions.normal.Normal(mean, lstd.exp())
-                project = torch.tensor(config.project).type_as(bbox_pred)
-                prob = q0.log_prob(project.repeat(mean.shape[0], 1))
-                prob_topk, _ = prob.exp().topk(config.reg_topk, dim=1)
-                prob_topk = prob_topk.reshape(N, H, W, 4, 4) * config.acc
-                stat = torch.cat([prob_topk, prob_topk.mean(dim=4, keepdim=True)], dim=4)
-                stat = stat.reshape(N, H, W, -1).permute(0, 3, 1, 2)
-            quality_score = self.reg_conf(stat)
-            cls_score = cls_score.sigmoid() * quality_score
-            pred_cls.append(cls_score)
-            pred_reg.append(bbox_pred)
-            pred_ctn.append(self.centerness_pred(self.bbox_subnet(feature)))
+            pred_cls.append(self.cls_score(self.cls_subnet(feature)))
+            pred_reg.append(self.bbox_pred(self.bbox_subnet(feature)))
         # reshape the predictions
         assert pred_cls[0].dim() == 4
         pred_cls_list = [
@@ -201,12 +160,9 @@ class RetinaNet_Head(nn.Module):
         pred_reg_list = [
             _.permute(0, 2, 3, 1).reshape(pred_reg[0].shape[0], -1, 8)
             for _ in pred_reg]
-        pred_ctn_list = [
-            _.permute(0, 2, 3, 1).reshape(pred_reg[0].shape[0], -1, 1)
-            for _ in pred_ctn]
-        return pred_cls_list, pred_reg_list, pred_ctn_list
+        return pred_cls_list, pred_reg_list
 
-def per_layer_inference(anchors_list, pred_cls_list, pred_reg_list, pred_ctn_list, im_info):
+def per_layer_inference(anchors_list, pred_cls_list, pred_reg_list, im_info):
     keep_anchors = []
     keep_scr = []
     keep_reg = []
@@ -215,8 +171,7 @@ def per_layer_inference(anchors_list, pred_cls_list, pred_reg_list, pred_ctn_lis
         anchors = anchors_list[l_id].reshape(-1, 4)
         pred_cls = pred_cls_list[l_id][0].reshape(-1, class_num)
         pred_reg = pred_reg_list[l_id][0].reshape(-1, 8)[:, :4]
-        pred_ctn = pred_ctn_list[l_id][0].reshape(-1, 1)
-        pred_scr = pred_cls * torch.sigmoid(pred_ctn)
+        pred_scr = torch.sigmoid(pred_cls)
         if len(anchors) > config.test_layer_topk:
             ruler = pred_scr.max(axis=1)[0]
             _, inds = ruler.topk(config.test_layer_topk, dim=0)
