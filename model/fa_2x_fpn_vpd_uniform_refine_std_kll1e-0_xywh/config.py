fd3e62a68cb172bc3c6d02cdaa5f35aa220e046c
gpu-4 <you@example.com>
2021-12-12 15:29:28 +0800

    1

--- model/fa_2x_fpn_vpd_uniform_refine_std_kll1e-0_xywh/config.py
+++ model/fa_2x_fpn_vpd_uniform_refine_std_kll1e-0_xywh/config.py
@@ -46,13 +46,13 @@ class Config:
     train_batch_per_gpu = 4
     momentum = 0.9
     weight_decay = 1e-4
-    base_lr = 3.125e-4
+    base_lr = 3.125e-4 * 2
     focal_loss_alpha = 0.25
     focal_loss_gamma = 2
 
     warm_iter = 800
-    max_epoch = 30
-    lr_decay = [24, 27]
+    max_epoch = 24
+    lr_decay = [16, 22]
     nr_images_epoch = 15000
     log_dump_interval = 20
 
@@ -81,10 +81,15 @@ class Config:
     positive_thresh = 0.5
     allow_low_quality = True
     save_data = False
+    add_test_noise = False
 
-    # -------------atss-config --------------------#
-    ignore_ioa_thr = 0.5
-    assign_topk = 9
-    kl_weight = 1.0
+    # ----------freeanchor config---------- #
+    bbox_thr = 0.6
+    pre_anchor_topk = 50
+    loss_box_alpha = 0.5
+    loss_box_gamma = 2.0
+    kl_weight = 0.5
     project = np.linspace(-1, 1, 21)[None, :]
+    stat_mode = 'std'
+    
 config = Config()
--- model/fa_2x_fpn_vpd_uniform_refine_std_kll1e-0_xywh/network.py
+++ model/fa_2x_fpn_vpd_uniform_refine_std_kll1e-0_xywh/network.py
@@ -72,7 +72,6 @@ class RetinaNet_Criteria(nn.Module):
     def __call__(self, pred_cls_list, pred_reg_list, anchors_list, gt_boxes, im_info):
         all_anchors = torch.cat(anchors_list, axis=0)
         all_pred_cls = torch.cat(pred_cls_list, axis=1).reshape(-1, config.num_classes-1)
-        all_pred_cls = torch.sigmoid(all_pred_cls)
         all_pred_dist = torch.cat(pred_reg_list, axis=1).reshape(-1, 8)
         # gaussian reparameterzation
         all_pred_mean = all_pred_dist[:, :4]
@@ -103,6 +102,11 @@ class RetinaNet_Head(nn.Module):
         super().__init__()
         num_convs = 4
         in_channels = 256
+        reg_channels = 64
+        if config.stat_mode == 'std':
+            ref_channels = 4
+        elif config.stat_mode == 'pdf':
+            ref_channels = 20
         cls_subnet = []
         bbox_subnet = []
         for _ in range(num_convs):
@@ -116,6 +120,13 @@ class RetinaNet_Head(nn.Module):
             bbox_subnet.append(nn.ReLU(inplace=True))
         self.cls_subnet = nn.Sequential(*cls_subnet)
         self.bbox_subnet = nn.Sequential(*bbox_subnet)
+
+        # refinement
+        conf_vector = [nn.Conv2d(ref_channels, reg_channels, 1)]
+        conf_vector += [nn.ReLU(inplace=True)]
+        conf_vector += [nn.Conv2d(reg_channels, 1, 1), nn.Sigmoid()]
+        self.reg_conf = nn.Sequential(*conf_vector)
+
         # predictor
         self.cls_score = nn.Conv2d(
             in_channels, config.num_cell_anchors * (config.num_classes-1),
@@ -125,7 +136,8 @@ class RetinaNet_Head(nn.Module):
             kernel_size=3, stride=1, padding=1)
 
         # Initialization
-        for modules in [self.cls_subnet, self.bbox_subnet, self.cls_score, self.bbox_pred]:
+        for modules in [self.cls_subnet, self.bbox_subnet, self.cls_score,
+                        self.bbox_pred, self.reg_conf]:
             for layer in modules.modules():
                 if isinstance(layer, nn.Conv2d):
                     torch.nn.init.normal_(layer.weight, mean=0, std=0.01)
@@ -139,8 +151,26 @@ class RetinaNet_Head(nn.Module):
         pred_cls = []
         pred_reg = []
         for feature in features:
-            pred_cls.append(self.cls_score(self.cls_subnet(feature)))
-            pred_reg.append(self.bbox_pred(self.bbox_subnet(feature)))
+            cls_score = self.cls_score(self.cls_subnet(feature))
+            bbox_pred = self.bbox_pred(self.bbox_subnet(feature))
+            # refinement
+            if config.stat_mode == 'std':
+                stat = bbox_pred[:, 4:]
+            elif config.stat_mode == 'pdf':
+                N, _, H, W = bbox_pred.size()
+                mean = bbox_pred[:, :4].permute(0,2,3,1).reshape(-1, 1)
+                lstd = bbox_pred[:, 4:].permute(0,2,3,1).reshape(-1, 1)
+                q0 = torch.distributions.normal.Normal(mean, lstd.exp())
+                project = torch.tensor(config.project).type_as(bbox_pred)
+                prob = q0.log_prob(project.repeat(mean.shape[0], 1))
+                prob_topk, _ = prob.exp().topk(config.reg_topk, dim=1)
+                prob_topk = prob_topk.reshape(N, H, W, 4, 4) * config.acc
+                stat = torch.cat([prob_topk, prob_topk.mean(dim=4, keepdim=True)], dim=4)
+                stat = stat.reshape(N, H, W, -1).permute(0, 3, 1, 2)
+            quality_score = self.reg_conf(stat)
+            cls_score = cls_score.sigmoid() * quality_score
+            pred_cls.append(cls_score)
+            pred_reg.append(bbox_pred)
         # reshape the predictions
         assert pred_cls[0].dim() == 4
         pred_cls_list = [
@@ -167,12 +197,12 @@ def per_layer_inference(anchors_list, pred_cls_list, pred_reg_list, im_info):
             _, inds = ruler.topk(config.test_layer_topk, dim=0)
             inds = inds.flatten()
             keep_anchors.append(anchors[inds])
-            keep_cls.append(torch.sigmoid(pred_cls[inds]))
+            keep_cls.append(pred_cls[inds])
             keep_reg.append(pred_reg[inds])
             keep_lstd.append(pred_lstd[inds])
         else:
             keep_anchors.append(anchors)
-            keep_cls.append(torch.sigmoid(pred_cls))
+            keep_cls.append(pred_cls)
             keep_reg.append(pred_reg)
             keep_lstd.append(pred_lstd)
     keep_anchors = torch.cat(keep_anchors, axis = 0)
