dc68ccdf21254d568c1f2b7fdbd67a27a30c2d48
ps <ps@ps>
2021-12-21 00:54:43 +0800

    1

--- model/atss_fpn_full_gau_kll1e-0_xywh/config.py
+++ model/atss_fpn_full_gau_kll1e-0_xywh/config.py
@@ -85,7 +85,7 @@ class Config:
     # -------------atss-config --------------------#
     ignore_ioa_thr = 0.5
     assign_topk = 9
-    kl_weight = 1e-2
+    kl_weight = 1.0
     project = np.linspace(-1, 1, 21)[None, :]
 
 config = Config()
