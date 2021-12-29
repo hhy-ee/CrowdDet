d4bb29352be1fcde1c379105a7019464dc88e73a
ps <ps@ps>
2021-12-30 02:01:38 +0800

    1

--- model/atss_fpn_jsgmm_kll1e-0_scale2_t1.0_xywh/config.py
+++ model/atss_fpn_jsgmm_kll1e-0_scale2_t1.0_xywh/config.py
@@ -85,7 +85,7 @@ class Config:
     # -------------atss-config --------------------#
     ignore_ioa_thr = 0.5
     assign_topk = 9
-    kl_weight = 2.0
+    kl_weight = 1.0
     gumbel_temperature = 1.0
     project = np.linspace(-2, 2, 21)[None, :]
 
